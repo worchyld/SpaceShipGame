8c4a552ab2f423a95cd2812a02279013e7386854
AD <silentflute.ad@gmail.com>
2024-09-05 19:25:34 +0100

    Cleanup

---
+++ SpaceShipGame/SpaceShipGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpaceShipGame
+//
+//  Created by Amarjit on 05/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpaceShipGame/SpaceShipGame/SpaceShipGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceShipGameApp.swift
+//  SpaceShipGame
+//
+//  Created by Amarjit on 05/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpaceShipGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
